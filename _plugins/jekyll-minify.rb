4a2f027b421aa08d931de10dca0767cbcf378b11
Arda Karaduman <akaraduman@gmail.com>
2017-05-26 16:28:17 +0900

    use minification

---
+++ _plugins/jekyll-minify.rb
@@ -0,0 +1,43 @@
+require "pry"
+require "reduce"
+
+def minify?
+  if Jekyll.env != "development" and Jekyll.configuration["s3_deploy"]
+    Jekyll.configuration["s3_deploy"]["minify"]
+  end
+end
+
+def compress?
+  if Jekyll.env != "development" and Jekyll.configuration["s3_deploy"]
+    Jekyll.configuration["s3_deploy"]["compress"]
+  end
+end
+
+def gzip(data)
+  sio = StringIO.new
+  gz = Zlib::GzipWriter.new(sio)
+  gz.write(data)
+  gz.close
+  sio.string
+end
+
+Jekyll::Hooks.register :site, :post_write do |site|
+  Dir.glob("#{Jekyll.configuration["destination"]}/**/*.*") do |file|
+    case File.extname(file)
+    when ".css", ".js", ".html", ".xml"
+      result = File.read file
+      result = Reduce.reduce(file) if minify?
+      result = gzip(result) if compress?
+      File.open(file, "w") do |f|
+        f.write(result)
+      end
+    when ".jpg", ".jpeg", ".gif", ".png"
+      result = file
+      result = Reduce.reduce(file) if minify?
+      result = gzip(result) if compress?
+      File.open(file, "w") do |f|
+        f.write(result)
+      end
+    end
+  end
+end
