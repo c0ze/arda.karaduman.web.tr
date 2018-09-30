24e6caee56089b70233f4c2beebf798cd2a1687f
Arda Karaduman <akaraduman@gmail.com>
2018-09-30 20:50:55 +0900

    force upload regadless of etag

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 
 hugo --baseURL https://arda.karaduman.web.tr/
 
-s3deploy -source public \
+s3deploy -force \
+	 -source public \
 	 -bucket ${AWS_BUCKET_NAME} \
 	 -key ${AWS_ACCESS_KEY} \
 	 -secret ${AWS_SECRET_KEY} \
