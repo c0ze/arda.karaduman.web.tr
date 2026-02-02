374d81a03f5916dd8bd76c57124d8ba7067ca561
Arda Karaduman <akaraduman@gmail.com>
2026-02-02 15:53:23 +0900

    Archive: add CNAME file for GitHub Pages

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-
-hugo --baseURL https://arda.karaduman.web.tr/
-
-s3deploy -force \
-	 -source public \
-	 -bucket ${AWS_BUCKET_NAME} \
-	 -key ${AWS_ACCESS_KEY} \
-	 -secret ${AWS_SECRET_KEY} \
-	 -region ${AWS_REGION}
